3a5c45ea676b788982408b53a2423519b3b334cf
atdifurio <atdifurio@gmail.com>
2019-03-01 12:20:23 -0500

    initial account creation

--- QSG.py
+++ QSG.py
@@ -1,16 +1,23 @@
-import json
-from flask import Flask, request, abort, url_for, redirect, session, render_template
-from flask_sqlalchemy import SQLAlchemy
+import os, datetime
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+#export FLASK_APP=salon.py
+from models import db, User
 
+# create our yuge application
 app = Flask(__name__)
+
+# Load default config and override config from an environment variable
 app.config.update(dict(
 	DEBUG=True,
 	SECRET_KEY='development key',
 	USERNAME='owner',
 	PASSWORD='pass',
+	SQLALCHEMY_TRACK_MODIFICATIONS = False,
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.root_path, 'salon.db')
 ))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+db.init_app(app)
 
 @app.cli.command('initdb')
 def initdb_command():
@@ -20,14 +27,26 @@ def initdb_command():
 	print('Initialized the database.')
 app.secret_key = "trashSecurity"
 
+def isUsernameUnique(username):
+	users = User.query.order_by(User.user_id.desc()).all()
+	print(users)
+	print(username)
+	if username in users:
+		print("false")
+		return True
+	return False
+
 @app.route("/accounts/<username>", methods=["GET", "POST"])
-def createAccount():
+def accounts():
+	return 404
+
+@app.route("/login/", methods=["GET", "POST"])
+def register():
 	if request.method == "POST":
 		if isUsernameUnique(request.form["username"]):
 			db.session.add(User(request.form["username"], request.form["password"]))
 			db.session.commit()
-
 			session["username"] = request.form["username"]
 			return redirect(url_for("profile", username=session["username"]))
-
+		return render_template('createAccount.html',unique=True)
 	return render_template("createAccount.html")
--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import datetime
 db = SQLAlchemy()
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(80), unique=True)
 	password = db.Column(db.String(80), unique=False)
 
